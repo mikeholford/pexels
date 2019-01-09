d6f2f5efe47b107ffdb90f92a4226118376e3426
Mike Holford <mike@holfy.io>
2019-01-09 22:07:29 +0000

    edit connection reference

--- lib/pexels/photo.rb
+++ lib/pexels/photo.rb
@@ -3,8 +3,6 @@ module Pexels
   class Photo
     attr_reader :id, :width, :height, :url, :photographer, :photographer_url
 
-    @connection = Pexels::Client.new.connect
-
     def initialize(attributes)
       @id = attributes["id"]
       @width = attributes["width"]
@@ -15,25 +13,29 @@ module Pexels
     end
 
     def self.get(id)
-      response = @connection.get "photos/#{id}"
+      connection = Pexels::Client.new.connect
+      response = connection.get "photos/#{id}"
       photo = JSON.parse(response.body)
       new(photo)
     end
 
     def self.search(query, per_page=15, page=1)
-      response = @connection.get "search?query=#{query}&per_page=#{per_page}&page=#{page}"
+      connection = Pexels::Client.new.connect
+      response = connection.get "search?query=#{query}&per_page=#{per_page}&page=#{page}"
       result = JSON.parse(response.body)
       result['photos'].map { |photo| new(photo) }
     end
 
     def self.curated(per_page=15, page=1)
-      response = @connection.get "curated?&per_page=#{per_page}&page=#{page}"
+      connection = Pexels::Client.new.connect
+      response = connection.get "curated?&per_page=#{per_page}&page=#{page}"
       result = JSON.parse(response.body)
       result['photos'].map { |photo| new(photo) }
     end
 
     def self.random
-      response = @connection.get "curated?&per_page=1&page=#{rand(1000)}"
+      connection = Pexels::Client.new.connect
+      response = connection.get "curated?&per_page=1&page=#{rand(1000)}"
       result = JSON.parse(response.body)
       new(result['photos'].last)
     end
