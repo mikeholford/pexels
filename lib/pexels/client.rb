9f0d0bd6a39b82d4c7bfb7538928dda9e29a4cc0
Mike Holford <mike@holfy.io>
2019-01-09 18:47:26 +0000

    clean up config

--- lib/pexels/client.rb
+++ lib/pexels/client.rb
@@ -1,15 +1,11 @@
 module Pexels
 
   class Client
-
     def connect
       api_endpoint = 'https://api.pexels.com/v1'
       # access_token = options.delete(:access_token) || raise('No Access Token given to Pexels Client')
       Faraday.new(:url => api_endpoint, :headers => {'Authorization' => "#{Pexels.configuration.access_token}"})
     end
-
-    # 563492ad6f91700001000001a9099b0e2801496db40fb7cfd0801845
-    # Pexels.configuration.access_token
   end
 
 end
