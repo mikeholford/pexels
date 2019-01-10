99c82610f4f01ab2a4c0afcb0de4eb05dc39d0e4
Mike Holford <mike@holfy.io>
2019-01-10 11:43:00 +0000

    config patch

--- lib/pexels/client.rb
+++ lib/pexels/client.rb
@@ -2,7 +2,7 @@ module Pexels
 
   class Client
     def connect(type)
-      Faraday.new(:url => api_endpoint(type), :headers => {'Authorization' => "563492ad6f91700001000001a9099b0e2801496db40fb7cfd0801845"})
+      Faraday.new(:url => api_endpoint(type), :headers => {'Authorization' => "#{Pexels.configuration.access_token}"})
     end
 
     def api_endpoint(type)
